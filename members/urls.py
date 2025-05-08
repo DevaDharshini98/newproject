5eb316da0ec711affdd8440f95d078015c600afb
DevaDharshini98 <devashc51@gmail.com>
2025-05-08 16:16:03 +0530

    first git

--- members/urls.py
+++ members/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.members, name='members'),
-    path('members/categories.html', views.members, name='memers'),  
+    path('', views.home_page, name='home_page'),
+    path('categories/', views.category_page, name='category_page'),
+    path('sale/', views.sale_page, name='sale_page'),
+    path('cart/', views.cart_page, name='cart_page'),
+    path('about/', views.about_page, name='about_page'),
 ]
